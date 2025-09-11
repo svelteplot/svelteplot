bc356a44f05609b8195e5ac030ed9b0fb3f1dea7
gka <gka@users.noreply.github.com>
2025-09-12 00:25:00 +0200

    docs: add more examples

---
+++ src/routes/examples/dot/dotx-from-array.svelte
@@ -0,0 +1,12 @@
+<script module>
+    export const title = 'Dots from number array';
+</script>
+
+<script lang="ts">
+    import { Plot, DotX } from 'svelteplot';
+    import { range } from 'd3-array';
+</script>
+
+<Plot grid inset={20}>
+    <DotX data={range(-10, 11).map((d) => d ** 3)} fill />
+</Plot>
