45432e0546cf429e5c1ffc4858c655c408f2dd49
gka <gka@users.noreply.github.com>
2025-09-12 14:53:00 +0200

    docs: box plot examples

---
+++ src/routes/examples/box/box-x.svelte
@@ -0,0 +1,22 @@
+<script module>
+    export const title = 'BoxX (horizontal)';
+</script>
+
+<script lang="ts">
+    import { Plot, BoxX } from 'svelteplot';
+    import { page } from '$app/state';
+    import type { ExamplesData } from '../types';
+
+    let { penguins } = $derived(page.data.data) as ExamplesData;
+</script>
+
+<Plot x={{ grid: true }}>
+    <BoxX
+        data={penguins}
+        x="flipper_length_mm"
+        y="species"
+        tickMinMax
+        dot={{ fill: true }}
+        bar={{ fill: 'var(--svelteplot-bg)', stroke: 'currentColor' }} />
+</Plot>
+
---
+++ src/routes/examples/box/box-y.svelte
@@ -0,0 +1,20 @@
+<script module>
+    export const title = 'BoxY (vertical)';
+</script>
+
+<script lang="ts">
+    import { Plot, BoxY } from 'svelteplot';
+    import { page } from '$app/state';
+    import type { ExamplesData } from '../types';
+
+    let { penguins } = $derived(page.data.data) as ExamplesData;
+</script>
+
+<Plot grid>
+    <BoxY
+        data={penguins}
+        x="species"
+        y="flipper_length_mm"
+        tickMedian={{ stroke: 'var(--svelteplot-bg)', strokeWidth: 3 }} />
+</Plot>
+
