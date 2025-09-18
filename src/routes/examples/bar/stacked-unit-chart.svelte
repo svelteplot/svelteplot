a54b62b57c72a08d841a3062a492ee0e7c8a0f2e
gka <gka@users.noreply.github.com>
2025-09-19 01:33:44 +0200

    improve example

--- src/routes/examples/bar/stacked-unit-chart.svelte
+++ src/routes/examples/bar/stacked-unit-chart.svelte
@@ -5,6 +5,9 @@
 <script>
     import { BarX, Plot, RuleX } from 'svelteplot';
     import { page } from '$app/state';
+    import { getContext } from 'svelte';
+    import { SVELTEPRESS_CONTEXT_KEY } from '@sveltepress/theme-default/context';
+    const { isDark } = getContext(SVELTEPRESS_CONTEXT_KEY);
     let { penguins } = $derived(page.data.data);
 </script>
 
@@ -12,7 +15,10 @@
     marginLeft={60}
     x={{ label: 'Frequency →' }}
     y={{ label: undefined }}
-    color={{ legend: true, scheme: 'reds' }}>
+    color={{
+        legend: true,
+        scheme: $isDark ? 'magma' : 'reds'
+    }}>
     <BarX
         data={penguins}
         y="species"
