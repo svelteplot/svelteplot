dbaa2cd3ec6fd8069ba538aad4565fdb68ee91cc
gka <gka@users.noreply.github.com>
2025-09-19 08:37:24 +0200

    formatting

--- src/routes/examples/bar/linked-bars.svelte
+++ src/routes/examples/bar/linked-bars.svelte
@@ -6,23 +6,12 @@
 
 <script>
     import { Plot, BarX } from 'svelteplot';
-    import { page } from '$app/state';
+
     const data = [
-        {
-            url: '/marks/line',
-            label: 'Line',
-            value: 5
-        },
-        {
-            url: '/marks/bar',
-            label: 'Bar',
-            value: 3
-        },
-        {
-            url: '/marks/dot',
-            label: 'Dot',
-            value: 4
-        }
+        { url: '/marks/line', label: 'Line', value: 5 },
+        { url: '/marks/bar', label: 'Bar', value: 3 },
+        { url: '/marks/dot', label: 'Dot', value: 4 },
+        { url: '/marks/rect', label: 'Rect', value: 2 }
     ];
 </script>
 
