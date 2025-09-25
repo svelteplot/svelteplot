e0c1d1479e4b45ded3d8b4e4da8a4e6651d34c2a
gka <gka@users.noreply.github.com>
2025-09-25 21:49:38 +0200

    fix: d3-regression import for repl

--- src/lib/marks/helpers/Regression.svelte
+++ src/lib/marks/helpers/Regression.svelte
@@ -43,7 +43,7 @@
         regressionLog,
         regressionPow,
         regressionLoess
-    } from '@gka/d3-regression';
+    } from '@gka/d3-regression/dist/d3-regression.esm.js';
     import { resolveChannel } from '$lib/helpers/resolve.js';
     import { confidenceInterval } from '$lib/helpers/math.js';
     import callWithProps from '$lib/helpers/callWithProps.js';
