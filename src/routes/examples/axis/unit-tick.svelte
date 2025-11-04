0695d3e195862e0bef16a97091d6f5b889f67364
gka <gka@users.noreply.github.com>
2025-11-04 14:54:56 +0100

    feat: pass ticks array to tickFormat function

---
+++ src/routes/examples/axis/unit-tick.svelte
@@ -0,0 +1,17 @@
+<script module>
+    export const title = 'Unit label tick format';
+</script>
+
+<script>
+    import { Plot, AxisX, AxisY, Line } from 'svelteplot';
+    import { page } from '$app/state';
+    let { aapl } = $derived(page.data.data);
+</script>
+
+<Plot
+    y={{
+        tickFormat: (d, i, ticks) =>
+            `${d}${i === ticks.length - 1 ? ' USD' : ''}`
+    }}>
+    <Line data={aapl} x="Date" y="Close" />
+</Plot>
