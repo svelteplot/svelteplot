a0441b6415ba5b5d0b6bbc712184a86e8db583c3
gka <gka@users.noreply.github.com>
2025-11-14 00:08:59 +0100

    example

---
+++ src/routes/examples/waffle/waffle.svelte
@@ -0,0 +1,57 @@
+<script module>
+    export let title = 'Simple Bars';
+    export let sortKey = 1;
+</script>
+
+<script>
+    import {
+        Plot,
+        WaffleY,
+        WaffleX,
+        RuleY
+    } from 'svelteplot';
+    import { Slider } from 'svelteplot/ui';
+    import { page } from '$app/state';
+    import RuleX from 'svelteplot/marks/RuleX.svelte';
+    let { alphabet } = $derived(page.data.data);
+
+    const data = [
+        { fruit: 'Apples', quantity: 212 },
+        { fruit: 'Bananas', quantity: 207 },
+        { fruit: 'Cherries', quantity: 315 },
+        { fruit: 'Dates', quantity: 11 }
+    ];
+
+    let gap = $state(1);
+    let unit = $state(1);
+</script>
+
+<Slider
+    label="Gap"
+    min={0}
+    max={10}
+    step={1}
+    bind:value={gap} />
+
+<Slider
+    label="Unit"
+    min={1}
+    max={10}
+    step={1}
+    bind:value={unit} />
+
+<Plot grid>
+    <RuleY data={[0]} />
+    <WaffleY
+        multiple={10}
+        {gap}
+        {unit}
+        {data}
+        x="fruit"
+        y="quantity" />
+</Plot>
+
+<Plot grid height={400}>
+    <RuleX data={[0]} />
+    <WaffleX {gap} {unit} {data} y="fruit" x="quantity" />
+</Plot>
