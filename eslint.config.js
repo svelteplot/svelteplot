0be8b6e5aa3b93891b276b9d1db7bd2c8a236ce6
gka <gka@users.noreply.github.com>
2026-02-09 00:05:45 +0100

    chore: update package.json information + add linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,21 @@ import svelte from 'eslint-plugin-svelte';
 import svelteParser from 'svelte-eslint-parser';
 import tsParser from '@typescript-eslint/parser';
 import * as regexpPlugin from 'eslint-plugin-regexp';
+import packageJson from 'eslint-plugin-package-json';
 
 export default [
     regexpPlugin.configs['flat/recommended'],
+    packageJson.configs.recommended,
     {
         rules: {
             'no-console': ['error', { allow: ['error'] }]
+        }
+    },
+    {
+        files: ['package.json'],
+        rules: {
+            'package-json/order-properties': 'error',
+            'package-json/sort-collections': 'error'
         }
     },
     // Ensure TypeScript files (including names like *.svelte.ts) use the TS parser
