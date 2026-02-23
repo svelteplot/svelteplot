9f88265e54a05da307acf93469e4a4a84727d95b
gka <gka@users.noreply.github.com>
2026-02-23 23:48:16 +0100

    remove old test plots

--- src/routes/tests/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<slot />
-
-<style>
-    :global(header.header) {
-        /* hide header so it doesn't mess with test screenshots */
-        display: none;
-    }
-</style>
--- src/routes/tests/line-facet-wrap/+page.md
+++
@@ -1,3 +0,0 @@
----
-title: Line Facet Wrapping
----
