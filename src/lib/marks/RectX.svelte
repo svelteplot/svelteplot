5773821075028d9f46479f254a83feca6e1475c9
gka <gka@users.noreply.github.com>
2026-02-25 00:50:40 +0100

    bring back default data in RectX and RectY

--- src/lib/marks/RectX.svelte
+++ src/lib/marks/RectX.svelte
@@ -26,7 +26,7 @@
     };
 
     const {
-        data = [],
+        data = [{} as Datum],
         stack,
         ...options
     }: RectXMarkProps = $derived({
--- src/lib/marks/RectY.svelte
+++ src/lib/marks/RectY.svelte
@@ -24,7 +24,7 @@
     };
 
     const {
-        data = [],
+        data = [{} as Datum],
         stack,
         ...options
     }: RectYMarkProps = $derived({
